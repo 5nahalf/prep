fad5cfce7d8ac71aec95c3b652ccc53966abc442
5nahalf <5nahalf@comcast.net>
2015-06-10 10:36:58 -0500

    basic routes set

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,3 +1,11 @@
 /**
  * Created by ErikJohnson on 6/10/15.
  */
+var Twitter = require('twitter');
+
+var client = new Twitter({
+    consumer_key: '',
+    consumer_secret: '',
+    access_token_key: '',
+    access_token_secret: ''
+});
