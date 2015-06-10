6b9875658b3d8252d39a514b19d6a00455c168c3
5nahalf <5nahalf@comcast.net>
2015-06-10 10:50:15 -0500

    changed version number in package.json

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -4,8 +4,9 @@
 var Twitter = require('twitter');
 
 var client = new Twitter({
-    consumer_key: '',
-    consumer_secret: '',
-    access_token_key: '',
-    access_token_secret: ''
+    //These values will need to be changed to match the prephoops twitter account.  Right now they are set to a app registered to @5nahalf
+    consumer_key: 'pu037E50fq109PqpCBQAh5mE0',
+    consumer_secret: 'MxFJDArOhaMOIrq0FOZjSnSHd2ONk4qykiybyBdiUasAF3yj72',
+    access_token_key: '2858804486-mvl0l34rTwG9wdrgdbBVvphfi4er7t9CbUiTbcj',
+    access_token_secret: 'jWaiyUNUbDFINUiRgiRtt17NjkDdJHpr9IyOzADkrz2pI'
 });
